f862b9e62df8c82deccd9162607d0f0d547bb699
Shahraz <workmshahraz@gmail.com>
2025-12-07 01:32:32 +0530

    Initial Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [vidFile, setVidFile] = useState()
 
   return (
-    <div className="upload-container">
-      <div className="upload-box">
-        <div className="upload-section">
-          
-        </div>
-      </div>
+    <div className='underline'>
+      Welcome
     </div>
   )
 }
